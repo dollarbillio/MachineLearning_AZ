59b12675264bfbc4694390e78611352dd2fa683a
DungNguyen <d.vietnguyen1011@gmail.com>
2018-10-21 02:23:10 +0700

    Done with DataPreprocessing

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -34,14 +34,13 @@ label_encoder_y = LabelEncoder()
 y = label_encoder_y.fit_transform(y)
 
 # Splitting the dataset into the Training set and Test set
-# from sklearn.cross_validation import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+# Feature Scaling (Example: Eucledian distance)
 from sklearn.preprocessing import StandardScaler
-'''
 sc_X = StandardScaler()
+# fit and transform
 X_train = sc_X.fit_transform(X_train)
+# sc_X already fit to X_train
 X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)'''
