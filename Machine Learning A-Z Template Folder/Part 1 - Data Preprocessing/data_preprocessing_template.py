e42a5ec208eeeea0d0e9f2fd697c04615ef25ac4
DungNguyen <d.vietnguyen1011@gmail.com>
2018-10-21 01:51:46 +0700

    Update the datapreprocessing_template

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -10,14 +10,38 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
+# Taking care of missing data
+# sklearn.preprocessing library
+# Crl + I for help
+from sklearn.preprocessing import Imputer
+
+# Create imputer object
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+# imputing the number columns
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+# Encoding Categorical
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+label_encoderX = LabelEncoder()
+X[:, 0] = label_encoderX.fit_transform(X[:, 0])
+# Dummy variables for categorical variables so that machine learning 
+# doesn't attribute order to categories
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+
+label_encoder_y = LabelEncoder()
+y = label_encoder_y.fit_transform(y)
+
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+# from sklearn.cross_validation import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
+'''
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train)'''
